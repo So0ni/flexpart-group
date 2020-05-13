3f22c9bff4468bab09a2b3a6f633e95b294cbf12
So0ni <173976914@qq.com>
2020-05-13 19:32:14 +0800

    fix install script

--- deps_install.sh
+++ deps_install.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+cd deps &&
 
 tar -zxf hdf5-1.8.21.tar.gz &&
 cd deps/hdf5-1.8.21 &&
 ./configure --prefix=/usr/local &&
-make && make install &&
+make && make install && cd .. &&
 
 tar -zxf netcdf-c-4.7.4.tar.gz &&
 cd ../netcdf-c-4.7.4 &&
 ./configure --prefix=/usr/local &&
-make && make install &&
+make && make install && cd .. &&
 
 tar -zxf netcdf-fortran-4.5.2.tar.gz &&
 cd ../netcdf-fortran-4.5.2 &&
 ./configure --prefix=/usr/local &&
-make && make install &&
+make && make install && cd .. &&
 
 tar -zxf eccodes-2.17.0-Source.gz &&
 cd ../build &&
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DENABLE_PYTHON=ON -DENABLE_FORTRAN=ON ../eccodes-2.17.0-Source &&
-make && make install &&
+make && make install && cd .. &&
 pip3 install --install-option="--prefix=/usr/local" eccodes 
